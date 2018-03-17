3cd368d12cdbb3b85e7521b61959404bf019792e
John Chua <jjchuadev@gmail.com>
2018-03-16 18:09:17 -0700

    Updated code to JSlint standards.

--- assets/js/pagescroller.js
+++ assets/js/pagescroller.js
@@ -1,67 +1,64 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
-
 $(document).ready(function () {
-  
-  $("#homeButton").on("click", function() {
+  $('#homeButton').on('click', function () {
     $('html, body').animate({
-        scrollTop: $("#intro").offset().top
+      scrollTop: $('#intro').offset().top,
     }, 1000);
   });
-	
-	$("#nameButton").on("click", function() {
+
+  $('#nameButton').on('click', function () {
     $('html, body').animate({
-        scrollTop: $("#intro").offset().top
+      scrollTop: $('#intro').offset().top,
     }, 1000);
   });
- 
-  
-  $("#aboutButton").on("click", function() {
+
+  $('#aboutButton').on('click', function () {
     $('html, body').animate({
-        scrollTop: $("#about-me").offset().top - 100
+      scrollTop: $('about-me').offset().top - 100,
     }, 1000);
   });
-	
-	$("#startButton").click(function() {
+
+  $('#startButton').on('click', function () {
     $('html, body').animate({
-        scrollTop: $("#about-me").offset().top - 100
+      scrollTop: $('#about-me').offset().top - 100,
     }, 1000);
   });
-  
-  $("#projectButton").on("click", function() {
+
+  $('#projectButton').on('click', function () {
     $('html, body').animate({
-        scrollTop: $("#projects").offset().top - 100
+      scrollTop: $('#projects').offset().top - 100,
     }, 1000);
   });
-  
-  $("#contactButton").on("click", function() {
+
+  $('#contactButton').on('click', function () {
     $('html, body').animate({
-        scrollTop: $("#contact").offset().top - 100
+      scrollTop: $('#contact').offset().top - 100,
     }, 1000);
   });
-	
-	$("#researchButton").on("click", function() {
+
+  $('#researchButton').on('click', function () {
     $('html, body').animate({
-        scrollTop: $("#userResearch").offset().top - 100
+      scrollTop: $('#userResearch').offset().top - 100,
     }, 1000);
   });
-	
-	$("#testingButton").on("click", function() {
+
+  $('#testingButton').on('click', function () {
     $('html, body').animate({
-        scrollTop: $("#userTesting").offset().top - 100
+      scrollTop: $('#userTesting').offset().top - 100,
     }, 1000);
   });
-	
-	$("#summaryButton").on("click", function() {
+
+  $('#summaryButton').on('click', function () {
     $('html, body').animate({
-        scrollTop: $("#summary").offset().top - 100
+      scrollTop: $('#summary').offset().top - 100,
     }, 1000);
   });
-	
-	$("#incorporationButton").on("click", function() {
+
+  $('#incorporationButton').on('click', function () {
     $('html, body').animate({
-        scrollTop: $("#incorporation").offset().top - 100
+      scrollTop: $('#incorporation').offset().top - 100,
     }, 1000);
   });
 });
